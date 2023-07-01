ee9115f07b8e09ffad38b22c382673fd8182876f
Pandit Abhishek <110360432+abhishek-io@users.noreply.github.com>
2023-07-01 17:17:33 +0000

    commit

--- hello.c
+++ hello.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    printf("hello world!\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+        printf("%s","*");
+        }
+        printf("\n");
+    }
+
 }
+
