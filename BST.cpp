833aa7dc6e5ec9344fbfa417618acf3257401010
Pandit Abhishek <110360432+abhishek-io@users.noreply.github.com>
2023-07-20 05:30:25 +0000

    c

---
+++ BST.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+struct BstNode{
+    int data;
+    BstNode* left;
+    BstNode* right;
+};
+
+int main(){
+    BstNode* root = NULL;
+    // cout<<"hello"<<endl;;
+}
